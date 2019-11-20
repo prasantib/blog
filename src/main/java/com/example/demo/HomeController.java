44bd4d44c107fa525a54e4b75906e3b10299e637
Prasanti Babu <prasanti.babu@gmail.com>
2019-11-20 00:36:26 -0500

    with more changes

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,23 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
-    @RequestMapping("/Blog")
+    @RequestMapping("/blog")
     public String blog(){
         return "Home";
     }
 
-    @RequestMapping("/BootCamp")
+    @RequestMapping("/bootCamp")
     public String bootcamp(){
         return "AboutTheBootCamp";
     }
 
-    @RequestMapping("/Blogger")
+    @RequestMapping("/blogger")
     public String blogger() {
         return "AboutTheBlogger";
     }
 
-    @RequestMapping("/Github")
+    @RequestMapping("/github")
     public String github() {
         return "github";
+    }
+
+    @RequestMapping("/github2")
+    public String github2() {
+        return "github2";
     }
 }
